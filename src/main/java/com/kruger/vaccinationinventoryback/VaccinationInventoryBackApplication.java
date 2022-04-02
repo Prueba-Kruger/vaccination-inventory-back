6e10932db1042c1442d5e27109d54416591b029b
romario <romario151996@gmail.com>
2022-04-02 12:02:58 -0500

    create project

---
+++ src/main/java/com/kruger/vaccinationinventoryback/VaccinationInventoryBackApplication.java
@@ -0,0 +1,13 @@
+package com.kruger.vaccinationinventoryback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationInventoryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationInventoryBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kruger/vaccinationinventoryback/VaccinationInventoryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruger.vaccinationinventoryback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationInventoryBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
